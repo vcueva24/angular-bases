abb480976a62f2a5d4ec4f691fe3261a37e532b8
Victor Cueva <victorcueva24@outlook.com>
2023-12-27 11:00:07 -0500

    Fin de sección 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
